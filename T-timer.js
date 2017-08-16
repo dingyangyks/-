5682d2ee6048718339fabeae1af6e8c20372ee23
dingyangyks <751706311@qq.com>
2017-08-17 00:03:41 +0800

    底边栏

--- T-timer.js
+++ T-timer.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', function () {
     nowTime();
     Move();
-
 })
 
 // 获取当前时间 设置初始位置
@@ -11,9 +10,9 @@ function nowTime() {
     var nowMinute = odate.getMinutes();
     var nowSecond = odate.getSeconds();
 
-    var secondHand = document.getElementById('second');
-    var minutesHand = document.getElementById('minutes');
-    var hourHand = document.getElementById('hour');
+    var secondHand = document.getElementsByClassName('second')[0];
+    var minutesHand = document.getElementsByClassName('minutes')[0];
+    var hourHand = document.getElementsByClassName('hour')[0];
 
     secondHand.style.transform = "rotate(" + nowSecond * 6 + "deg)";
     minutesHand.style.transform = "rotate(" + nowMinute * 6 + "deg)";
@@ -22,9 +21,9 @@ function nowTime() {
 
 // 指针
 function Move() {
-    var secondHand = document.getElementById('second');
-    var minutesHand = document.getElementById('minutes');
-    var hourHand = document.getElementById('hour');
+    var secondHand = document.getElementsByClassName('second')[0];
+    var minutesHand = document.getElementsByClassName('minutes')[0];
+    var hourHand = document.getElementsByClassName('hour')[0];
 
     setInterval(function () {
         // var odate = new Date('Wed Aug 16 2017 23:00:15 GMT+0800 (CST)');
@@ -37,12 +36,11 @@ function Move() {
         var PerMinute = nowMinute * 6;
         var PerHour = nowHour * 30;
 
-        // PerSecond += 6;
-        // PerMinute += 0.1;
-        // PerHour += 360 / 86400;
         secondHand.style.transform = "rotate(" + PerSecond % 360 + "deg)";
         minutesHand.style.transform = "rotate(" + PerMinute % 360 + "deg)";
         hourHand.style.transform = "rotate(" + PerHour % 360 + "deg)";
     }, 1000)
 }
+
+// 秒表
 
